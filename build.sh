89c752287393f149e34a9e4c7d37310328fa8eee
sakkke <w32w64@gmail.com>
2022-01-30 15:12:23 +0900

    fix issue that sorting is not applied

--- build.sh
+++ build.sh
@@ -17,12 +17,12 @@ rm -fr "$TARGET" configs/_termy
 mkdir "$TARGET" configs/_termy
 
 for profile in ${profiles[@]}; do
-  cp -RT "$(realpath "configs/$profile")" "$TARGET"
-  if [[ $profile != _termy ]] && [[ -f configs/$profile/packages.x86_64 ]]; then
+  if [[ $profile = _termy ]]; then
+    sort -uo configs/_termy/packages.x86_64{,}
+  elif [[ -f configs/$profile/packages.x86_64 ]]; then
     cat "configs/$profile/packages.x86_64" >> configs/_termy/packages.x86_64
   fi
+  cp -RT "$(realpath "configs/$profile")" "$TARGET"
 done
-
-sort -uo configs/_termy/packages.x86_64{,}
 
 mkarchiso -v "$TARGET"
