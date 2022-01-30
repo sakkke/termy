58fefa24bdb106817a06a6d6dbd0e51945027d15
sakkke <w32w64@gmail.com>
2022-01-30 10:57:55 +0900

    fix issue that file is be overwritten every time

--- build.sh
+++ build.sh
@@ -11,11 +11,18 @@ while getopts p: OPT; do
   esac
 done
 
-rm -fr "$TARGET"
-mkdir "$TARGET"
+profiles+=(_termy)
+
+rm -fr "$TARGET" configs/_termy
+mkdir "$TARGET" configs/_termy
 
 for profile in ${profiles[@]}; do
   cp -RT "$(realpath "configs/$profile")" "$TARGET"
+  if [[ $profile != _termy ]] && [[ -f configs/$profile/packages.x86_64 ]]; then
+    cat "configs/$profile/packages.x86_64" >> configs/_termy/packages.x86_64
+  fi
 done
+
+sort -uo configs/_termy/packages.x86_64{,}
 
 mkarchiso -v "$TARGET"
