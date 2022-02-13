3325858108e3e6bb432fb442250fca1cb1da360c
sakkke <w32w64@gmail.com>
2022-02-13 20:48:35 +0900

    remove helper scripts

--- clean.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-TARGET=termy
-rm -fr "$TARGET" configs/_termy
--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source configs/live/profiledef.sh
-image_name="$iso_name-$iso_version-$arch.iso"
-run_archiso -i "termy/out/$image_name" $@
